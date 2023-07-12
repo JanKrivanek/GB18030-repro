41569280605a5c5dd32efeb8df9cf40d5a35e07b
Jan Krivanek <jankrivanek@microsoft.com>
2023-07-12 17:23:49 +0200

    Initial commit

---
+++ D5U1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D5_____U1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
